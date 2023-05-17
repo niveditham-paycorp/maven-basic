2fb0f7234f5902a9e7a6faae3b7a63e1bbb57a1a
niveditham-paycorp <133314514+niveditham-paycorp@users.noreply.github.com>
2023-05-17 10:23:53 +0530

    Add files via upload

---
+++ bank/src/main/java/AmountWithdrawn/bank/App.java
@@ -0,0 +1,23 @@
+package AmountWithdrawn.bank;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        
+        try {
+        Connection conn = DriverManager.getConnection(
+                "jdbc:mysql://127.0.0.1:3306/test", "root", "password"); 
+        }
+        	catch (Exception e) {
+                e.printStackTrace();
+    }
+}
+}
